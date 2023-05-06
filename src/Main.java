6f272cbb4614f7e7150b109ab92f2e715eaf8a62
Ansar Ismagilov <ansar89kab@bk.ru>
2023-05-06 10:06:50 +0700

    in the process 3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {-1, 0, 1, 2, -1, -4};
-        solution.threeSum(nums);
-
-
+        int[] nums = {0,3,0,1,1,-1,-5,-5,3,-3,-3,0};
+        System.out.println(solution.threeSum(nums).toString());
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,18 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+// -4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> triplets = new List<List<Integer>>;
-
-        for (int i = 1; i < nums.length - 1; i++) {
-            if (nums[0] != nums[i] &&
-                    nums[0] != nums[i + 1] &&
-                    nums[i] != nums[i + 1]) {
-                if (nums[0] + nums[i] + nums[i + 1] = 0) {
-                    //check for uniqueness with triplets
-                }
+        List<List<Integer>> triplets = new ArrayList<>();
+        for (int j = 0; j < nums.length; j++) {
+            for (int i = 0; i < nums.length; i++) {
+                for (int k = 0; k < nums.length; k++) {
+                    if (j != i && k != i && j != k) {
+                        if (nums[j] + nums[i] + nums[k] == 0) {
+                            boolean isUnique = checker(nums[j], nums[i], nums[k], triplets);
+                            if (isUnique)
+                                triplets.add(
+                                        new ArrayList<>(
+                                                Arrays.asList(nums[j], nums[i], nums[k])));
+                        }
+                    }
                 }
             }
         }
+        return triplets;
+    }
+
+    public boolean checker(int a, int b, int c, List<List<Integer>> triplets) {
+        boolean res = true;
+        if (triplets.isEmpty()) return res;
+        for (int i = 0; i < triplets.size(); i++) {
+            List<Integer> list = triplets.get(i);
+            int count = 0;
+            for (var num : list) {
+                if (num == a || num == b || num == c) count++;
+            }
+            if (count == 3) res = false;
+        }
+        return res;
     }
 }
